61c25e00c589dd46f2b32a2558df16aff1f5d411
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-15 18:44:54 -0400

    Refactor header.php to add mini and large logo

--- views/modules/header.php
+++ views/modules/header.php
@@ -10,7 +10,25 @@
         </span>
 
         <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg">
+
+            <img src="views/img/template/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+
+        </span>
 
     </a>
+
+    <!-- Header Navbar -->
+
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+
+            <span class="sr-only">Toggle navigation</span>
+
+        </a>
+
+    </nav>
 
 </header>
