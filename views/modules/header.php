f7fe80dce644719c38c4aa3e272e6e10fd6072f3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-15 18:05:39 -0400

    Refactor header.php to add logo and improve organization

---
+++ views/modules/header.php
@@ -0,0 +1,16 @@
+<header class="main-header">
+
+    <a href="" class="logo">
+
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini">
+
+            <img src="" alt="Logo">
+
+        </span>
+
+        <!-- logo for regular state and mobile devices -->
+
+    </a>
+
+</header>
