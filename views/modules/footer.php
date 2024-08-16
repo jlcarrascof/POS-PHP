7dfc1bb0dd6174a77acf87ebd29b56eae344809a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-16 10:59:09 -0400

    Refactor footer.php to fix copyright link formatting

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2024 <a href="https://github.com/jlcarrascof" target="_blank">Administrative Solutions, S.A.</a>.</strong>
+    <strong>Copyright &copy; 2014-2024 <a href="https://github.com/jlcarrascof" target="_blank">Administrative Solutions, S.A.</a></strong>
     All rights reserved.
 </footer>
