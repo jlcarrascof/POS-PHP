bdb94f25497defee9dee6939edc20c4de12c1b73
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-15 23:35:56 -0400

    Refactor menu.php to add user-related links in sidebar

--- views/modules/menu.php
+++ views/modules/menu.php
@@ -8,6 +8,30 @@
                     <span>Home</span>
                 </a>
             </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user"></i>
+                    <span>Users</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-th"></i>
+                    <span>Categories</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-product-hunt"></i>
+                    <span>Products</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-users"></i>
+                    <span>Customers</span>
+                </a>
+            </li>
         </ul>
     </section>
 
