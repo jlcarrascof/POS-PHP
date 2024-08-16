40bfbe0609b4cbd2d9484887d1e0c02c44a2e3a5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-15 23:21:53 -0400

    Refactor menu.php to add home link in sidebar

--- views/modules/menu.php
+++ views/modules/menu.php
@@ -1,7 +1,14 @@
 <aside class="main-sidebar">
 
     <section class="sidebar">
-
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a href="">
+                    <i class="fa fa-home"></i>
+                    <span>Home</span>
+                </a>
+            </li>
+        </ul>
     </section>
 
 </aside>
