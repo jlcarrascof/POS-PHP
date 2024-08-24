ef817247b8e0edd8fb9abe682fc8c1507d925590
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-23 22:25:25 -0400

    Refactor controllers and templates for improved structure and organization

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 require_once 'controllers/template.controller.php';
+require_once 'controllers/categories.controller.php';
+require_once 'controllers/customers.controller.php';
+require_once 'controllers/products.controller.php';
+require_once 'controllers/sales.controller.php';
+require_once 'controllers/users.controller.php';
 
 $template = new ControllerTemplate();
 $template->ctrTemplate();
