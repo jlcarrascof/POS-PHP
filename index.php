990d95e037df9ea49f4f9152cb4ee72860fca4da
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-23 22:29:24 -0400

    Refactor index.php to include necessary models for categories, customers, products, sales, and users

--- index.php
+++ index.php
@@ -7,5 +7,11 @@ require_once 'controllers/products.controller.php';
 require_once 'controllers/sales.controller.php';
 require_once 'controllers/users.controller.php';
 
+require_once 'models/categories.model.php';
+require_once 'models/customers.model.php';
+require_once 'models/products.model.php';
+require_once 'models/sales.model.php';
+require_once 'models/users.model.php';
+
 $template = new ControllerTemplate();
 $template->ctrTemplate();
