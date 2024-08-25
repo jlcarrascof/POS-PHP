f9c1afcf767bba34b47545fc740c7aced894c238
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-24 20:41:36 -0400

    Refactor 404 page to update dashboard link

--- views/modules/404.php
+++ views/modules/404.php
@@ -26,7 +26,7 @@
           </h3>
           <p>
             We could not find the page you were looking for.
-            Meanwhile, you may <a href="index.php">return to dashboard</a> or try using the search form.
+            Meanwhile, you may <a href="home">return to dashboard</a> or try using the search form.
           </p>
         </div>
       </div>
