305f4ce89ba1a1074afb3a73a0cd035988176e9f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-24 20:34:33 -0400

    Refactor 404 page to update breadcrumb and error message

--- views/modules/404.php
+++ views/modules/404.php
@@ -7,7 +7,7 @@
 
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Admin Customers</li>
+        <li class="active">Page Not Found</li>
       </ol>
 
     </section>
@@ -15,29 +15,21 @@
     <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+      <!-- Your Page Content Here -->
+      <div class="error-page">
+        <h2 class="headline text-primary">404</h2>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+        <div class="error-content">
+          <h3>
+            <i class="fa fa-warning text-primary"></i>
+            Oops! Page not found.
+          </h3>
+          <p>
+            We could not find the page you were looking for.
+            Meanwhile, you may <a href="index.php">return to dashboard</a> or try using the search form.
+          </p>
         </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
       </div>
-      <!-- /.box -->
 
     </section>
     <!-- /.content -->
