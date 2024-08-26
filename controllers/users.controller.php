ef1b74fe61d35dda1118ab898c0100a0dcc65d23
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 11:18:16 -0400

    Refactor Validate ingUser and ingPassword in login form

--- controllers/users.controller.php
+++ controllers/users.controller.php
@@ -1,1 +1,21 @@
 <?php
+
+class UsersController {
+
+    public function ctrLoginUser() {
+
+        // ingUser and ingPassword
+
+        if (isset($_POST["ingUser"])) {
+
+            if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUser"]) &&
+                preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])) {
+
+                 $table = "users";
+            }
+
+        }
+
+    }
+
+}
