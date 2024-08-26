ffdc4f0891e7d5fd8ba058c748afdc561b084ad9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 18:39:00 -0400

    chore: Refactor UsersModel to use prepared statements for retrieving users

--- models/users.model.php
+++ models/users.model.php
@@ -6,6 +6,9 @@ class UsersModel {
 
     // SHOW USERS
     static public function mdlShowUsers($table, $item, $value) {
-        $stmt = Connection::connect()->prepare();
+        $stmt = Connection::connect()->prepare("SELECT * FROM $table WHERE $item = :$item");
+        $stmt->bindParam(":".$item, $value, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch();
     }
 }
