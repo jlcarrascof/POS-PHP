09058b586147c996fe51cb68581135e526aaa4d7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-07 15:03:11 -0400

    Refactor categories.php module to update modal for adding categories

--- views/modules/categories.php
+++ views/modules/categories.php
@@ -109,38 +109,38 @@
 </div>
 
 <!--=====================================
-MODAL AGREGAR USUARIO
+MODAL ADD CATEGORY
 ======================================-->
 
-<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+<div id="modalAddCategory" class="modal fade" role="dialog">
 
   <div class="modal-dialog">
 
     <div class="modal-content">
 
-      <form role="form" method="post" enctype="multipart/form-data">
+      <form role="form" method="post">
 
         <!--=====================================
-        CABEZA DEL MODAL
+        MODAL HEADER
         ======================================-->
 
         <div class="modal-header" style="background:#3c8dbc; color:white">
 
           <button type="button" class="close" data-dismiss="modal">&times;</button>
 
-          <h4 class="modal-title">Agregar usuario</h4>
+          <h4 class="modal-title">Add Category</h4>
 
         </div>
 
         <!--=====================================
-        CUERPO DEL MODAL
+        MODAL BODY
         ======================================-->
 
         <div class="modal-body">
 
           <div class="box-body">
 
-            <!-- ENTRADA PARA EL NOMBRE -->
+            <!-- INPUTS -->
 
             <div class="form-group">
 
@@ -148,75 +148,9 @@ MODAL AGREGAR USUARIO
 
                 <span class="input-group-addon"><i class="fa fa-user"></i></span>
 
-                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+                <input type="text" class="form-control input-lg" name="newCategory" placeholder="Input category" required>
 
               </div>
-
-            </div>
-
-            <!-- ENTRADA PARA EL USUARIO -->
-
-             <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-key"></i></span>
-
-                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" required>
-
-              </div>
-
-            </div>
-
-            <!-- ENTRADA PARA LA CONTRASEÑA -->
-
-             <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-
-                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar contraseña" required>
-
-              </div>
-
-            </div>
-
-            <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
-
-            <div class="form-group">
-
-              <div class="input-group">
-
-                <span class="input-group-addon"><i class="fa fa-users"></i></span>
-
-                <select class="form-control input-lg" name="nuevoPerfil">
-
-                  <option value="">Selecionar perfil</option>
-
-                  <option value="Administrador">Administrador</option>
-
-                  <option value="Especial">Especial</option>
-
-                  <option value="Vendedor">Vendedor</option>
-
-                </select>
-
-              </div>
-
-            </div>
-
-            <!-- ENTRADA PARA SUBIR FOTO -->
-
-             <div class="form-group">
-
-              <div class="panel">SUBIR FOTO</div>
-
-              <input type="file" id="nuevaFoto" name="nuevaFoto">
-
-              <p class="help-block">Peso máximo de la foto 200 MB</p>
-
-              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
 
             </div>
 
